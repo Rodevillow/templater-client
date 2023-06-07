478ed236b422c1da082252a00d545d9ef74dfcc9
rodionmaksimenko <asvrodya@gmail.com>
2023-06-08 00:00:44 +0300

    Add wrapper test class for test pull request

--- app.vue
+++ app.vue
@@ -1,7 +1,9 @@
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <div class="wrapper-test">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
 </template>
 
 <script lang="ts">
