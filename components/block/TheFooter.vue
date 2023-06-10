2b4ad0be3f9c4735ce1fdbb6bb634eed98eb5379
deceser <aufhaummtobsc@gmail.com>
2023-06-11 01:02:05 +0300

    added new folder: block | TODO: create header and footer

---
+++ components/block/TheFooter.vue
@@ -0,0 +1,10 @@
+<template>
+  <footer>
+    <div class="">
+      <div class=""></div>
+      <div class=""></div>
+    </div>
+  </footer>
+</template>
+<script lang="ts"></script>
+<style lang="scss"></style>
---
+++ components/block/TheHeader.vue
@@ -0,0 +1,10 @@
+<template>
+  <header>
+    <div class="">
+      <div class=""></div>
+      <div class=""></div>
+    </div>
+  </header>
+</template>
+<script lang="ts"></script>
+<style lang="scss"></style>
