a595e569ae7346da3c7e8c57ad8809bda4a53df8
deceser <aufhaummtobsc@gmail.com>
2023-06-14 23:13:04 +0300

    Added basic password validation logic

--- components/ui/UiButtonPrimary.vue
+++ components/ui/UiButtonPrimary.vue
@@ -1,9 +1,16 @@
 <template>
-  <button class="btn btn-primary">
+  <button class="btn btn-primary" :type="props.type">
     <slot />
   </button>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const props = defineProps({
+  type: {
+    type: String as PropType<"button" | "submit" | "reset" | undefined>,
+    default: undefined,
+  },
+});
+</script>
 
 <style lang="scss" scoped></style>
