781078dc7f90686d180e82d29462d610be8d99e4
deceser <aufhaummtobsc@gmail.com>
2023-08-11 15:20:37 +0300

    Created base example QRcode (in progress)

---
+++ modules/qr-code/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <qrcode-vue :value="value" :level="props.level" :renderAs="props.renderAs" :size="props.size" :background="props.bg" />
+</template>
+
+<script setup lang="ts">
+import QrcodeVue, { Level, RenderAs } from "qrcode.vue";
+import { PropType } from "vue";
+
+export interface QrCodeProps {
+  bg: string;
+  size: number;
+  value: string;
+  level: Level;
+  renderAs: RenderAs;
+}
+
+const props: QrCodeProps = defineProps({
+  value: {
+    type: String as PropType<string>,
+    required: true,
+  },
+
+  level: {
+    type: String as PropType<Level>,
+    default: "H",
+  },
+
+  renderAs: {
+    type: String as PropType<RenderAs>,
+    default: "svg",
+  },
+
+  size: {
+    type: Number as PropType<number>,
+    required: true,
+  },
+
+  bg: {
+    type: String as PropType<string>,
+    required: true,
+  },
+});
+</script>
---
+++ pages/qr-code/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <QrCodeModule
+      :value="qrCodeValue"
+      :level="qrCodeLevel"
+      :renderAs="qrCodeRenderAs"
+      :size="qrCodeSize"
+      :bg="qrCodeBackground"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import QrCodeModule from "@/modules/qr-code/index.vue";
+
+const qrCodeValue = "https://ru.wikipedia.org/wiki/%D0%A7%D0%BB%D0%B5%D0%BD";
+const qrCodeLevel = "H"; // error correction level "H"
+const qrCodeRenderAs = "svg"; // render format "svg"
+const qrCodeSize = 200;
+const qrCodeBackground = "#43998b";
+</script>
+
+<style scoped lang="scss"></style>
