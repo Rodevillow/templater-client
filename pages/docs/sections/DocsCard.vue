bc120e8c31b0dab2c2248c54ec2eb9bcc423c9cf
deceser <aufhaummtobsc@gmail.com>
2023-10-23 16:15:03 +0300

    Added feat/docs-card

---
+++ pages/docs/sections/DocsCard.vue
@@ -0,0 +1,9 @@
+<template>
+	<CardDefault title="item.name" description="item.description" />
+</template>
+
+<script setup lang="ts">
+import CardDefault from '~/components/block/card/CardDefault.vue'
+</script>
+
+<style scoped></style>
