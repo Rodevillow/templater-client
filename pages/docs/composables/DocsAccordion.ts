2f7011f6a4f45d3eb92087197c3589a7b58d2e89
rodionmaksimenko <asvrodya@gmail.com>
2023-11-13 01:29:28 +0200

    Rewrite UiAccordion.vue

--- pages/docs/composables/DocsAccordion.ts
+++ pages/docs/composables/DocsAccordion.ts
@@ -1,5 +1,24 @@
 export const codeExampleAccordion = `
-<UiAccordion :title="your title" :descrption="your description" />
+<UiAccordion 
+    :data="[
+    {
+        isActive: false,
+        title: 'Some test title 1',
+        description: 'Some test description 1',
+      },
+      {
+        isActive: false,
+        title: 'Some test title 2',
+        description: 'Some test description 2',
+      },
+      {
+        isActive: false,
+        title: 'Some test title 3',
+        description: 'Some test description 3',
+      },
+    ]"
+    @change="handleAccordionChange" 
+/>
 `
 
 export default codeExampleAccordion
--- pages/docs/sections/DocsAccordion.vue
+++ pages/docs/sections/DocsAccordion.vue
@@ -1,7 +1,28 @@
 <template>
-	<UiAccordion />
+	<UiAccordion :data="testData" @change="handleAccordionChange" />
 </template>
 
 <script setup lang="ts">
 import UiAccordion from '~/components/ui/UiAccordion.vue'
+import IAccordionItem from "~/composables/interfaces/IAccordeonItem";
+
+let testData = reactive([
+  {
+    isActive: false,
+    title: "Some test title 1",
+    description: "Some test description 1",
+  },
+  {
+    isActive: false,
+    title: "Some test title 2",
+    description: "Some test description 2",
+  },
+  {
+    isActive: false,
+    title: "Some test title 3",
+    description: "Some test description 3",
+  },
+]);
+
+const handleAccordionChange = (changedData: Array<IAccordionItem>) => testData = changedData;
 </script>
